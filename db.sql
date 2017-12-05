d0edb9f81f06ef4270408ca90005086d99c6ec5a
a-kap95 <34221273+a-kap95@users.noreply.github.com>
2017-12-04 19:42:39 -0600

    db file new

--- db.sql
+++ db.sql
@@ -1,1 +1,2 @@
 create table if not exists evehicle (id INTEGER PRIMARY KEY AUTOINCREMENT,type INTEGER,zipcode INTEGER);
+create table if not exists vrequest (id INTEGER PRIMARY KEY AUTOINCREMENT,type INTEGER,zipcode INTEGER,vid INTEGER)
