bf30a79e9e62b220c22bb95c3b71702c6533d399
a-kap95 <34221273+a-kap95@users.noreply.github.com>
2017-12-04 20:48:37 -0600

    init_db update 2

--- init_db.py
+++ init_db.py
@@ -9,5 +9,8 @@ def init_db():
     print(sql)
     db.cursor().execute(sql)
     db.commit()
+	sql1="create table if not exists vrequest (id INTEGER PRIMARY KEY AUTOINCREMENT,type INTEGER,zipcode INTEGER,vid INTEGER);"
+    db.cursor().execute(sql1)
+    db.commit()
 if __name__ == '__main__':
     init_db()
